6564830b76ad555f63c8730a7c4e47aeb680f679
faizan <FAIZANNAGHMAN@GMAIL.COM>
2023-10-02 03:05:17 +0500

    add tests for endpoints

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,61 +1,51 @@
-# This is test_app.py
-
-import json
+from app import app_with_scraper
 import pytest
-from app import app, imported_products  # import your app and your imported_products list
+
+app_with_scraper.config['TESTING'] = True
 
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
+    with app_with_scraper.test_client() as client:
         yield client
 
 
-def test_import_product(client):
-    payload = {'url': 'https://www.trendyol.com/rise-and-shine/keratin-kolajen-sac-bakim-sutu-200-ml-p-174950884'}
-    response = client.post('/import_product', json=payload)
+@pytest.fixture
+def trendyol_urls():
+    urls = [
+        "https://www.trendyol.com/sail-lakers/beyaz-deri-bagcikli-erkek-gunluk-ayakkabi-p-366825717",
+        "https://www.trendyol.com/gelincik/kahvaltilik-surulebilir-dogal-katkisiz-10kg-ozel-uretim-sekersiz-fistik-ezmesi-krem-p-234064644",
+        "https://www.trendyol.com/zeyder-kids/kiz-bebek-baskili-t-shirt-p-731763902",
+        "https://www.trendyol.com/nuclear/bubba-juice-10-ml-mix-aroma-kapruz-sakiz-p-731515507",
+        "https://www.trendyol.com/midday/2-adet-evinize-somine-havasini-yasatacak-olan-dekoratif-5-watt-alev-gorunumlu-led-alevli-ampul-lamba-p-474594130",
+        "https://www.trendyol.com/dk-tuning/tesla-model-3-s-x-orjinal-jant-gobek-kapagi-seti-57mm-gumus-renk-p-732527982",
+        "https://www.trendyol.com/trendshopping/iphone-13-kose-korumali-antik-deri-telefon-kilifi-lacivert-p-348328113",
+        "https://www.trendyol.com/opinel/inox-trekking-8-no-paslanmaz-celik-caki-p-378505993",
+        "https://www.trendyol.com/victoria-s-secret/bej-rengi-kadin-kulot-victoria-s-secret-brazilian-kesim-bej-kadin-kulot-hediyelik-hediye-kulot-p-466727358",
+        "https://www.trendyol.com/lema-store/3-yas-rakam-ve-20-adet-makaron-balon-sari-beyaz-p-656012721",
+        "https://www.trendyol.com/bambi/vizon-suet-kadin-cizme-k03828030102-p-163949261"
+    ]
+    return urls
+
+
+def test_scrape_endpoint(client, trendyol_urls):
+    """
+    Test that the scrape endpoint returns a list of products
+    """
+    # Mock data for testing
+    mock_urls = {
+        "urls": trendyol_urls
+    }
+    response = client.post('/scrape', json=mock_urls)
     assert response.status_code == 200
-    assert len(imported_products) == 1  # The list should contain one product now
+    assert len(response.get_json()) == len(trendyol_urls)
 
 
-def test_import_product_invalid_url(client):
-    payload = {'url': 'https://invalid.com/some-product'}
-    response = client.post('/import_product', json=payload)
-    assert response.status_code == 400
-    assert b'Invalid URL' in response.data
-
-
-def test_delete_product(client):
-    # First, add a product for testing deletion
-    payload = {'url': 'https://www.trendyol.com/morfose/milk-therapy-fon-sutu-400-ml-p-4780060'}
-    client.post('/import_product', json=payload)
-    delete_payload = {'url': 'https://www.trendyol.com/morfose/milk-therapy-fon-sutu-400-ml-p-4780060'}
-    response = client.post('/delete_product', json=delete_payload)
+def test_generate_csv_endpoint(client, trendyol_urls):
+    """ Test that the generate-csv endpoint returns a CSV file """
+    mock_urls = {
+        "urls": trendyol_urls
+    }
+    response = client.post('/generate-csv', json=mock_urls)
     assert response.status_code == 200
-    # todo determine why the product is not deleted from the global list
-    assert len(imported_products) == 0  # The list should be empty now
-
-
-def test_reset_list(client):
-    # First, add some products for testing reset
-    payload1 = {'url': 'https://www.trendyol.com/rise-and-shine/keratin-kolajen-sac-bakim-sutu-200-ml-p-174950884'}
-    payload2 = {'url': 'https://www.trendyol.com/morfose/milk-therapy-fon-sutu-400-ml-p-4780060'}
-    client.post('/import_product', json=payload1)
-    client.post('/import_product', json=payload2)
-
-    response = client.get('/reset_list')
-    assert response.status_code == 200
-    assert len(imported_products) == 0  # The list should be empty now
-
-
-def test_export_csv(client):
-    # First, add some products for testing export
-    payload1 = {'url': 'https://www.trendyol.com/morfose/milk-therapy-fon-sutu-400-ml-p-4780060'}
-    payload2 = {'url': 'https://www.trendyol.com/rise-and-shine/keratin-kolajen-sac-bakim-sutu-200-ml-p-174950884'}
-    client.post('/import_product', json=payload1)
-    client.post('/import_product', json=payload2)
-
-    response = client.get('/export_csv')
-    assert response.status_code == 200
-    assert b'CSV exported successfully' in response.data
+    assert response.headers['Content-Disposition'] == 'attachment; filename=products.csv'
