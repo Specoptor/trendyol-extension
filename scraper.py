3ac90166e07abd72c06230225fd64ec2ebb43c28
faizan <FAIZANNAGHMAN@GMAIL.COM>
2023-10-01 19:10:58 +0500

    update the comments and add a method to scrape the barcode.
    Barcode is found in a script tag. A regex pattern scans the text inside script tags to find the barcode. If found, the barcode is returned, else return None.

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+import re
+
 from selenium.webdriver.common.by import By
 from driver import Driver
 import logging
@@ -8,6 +10,11 @@ logger = logging.Logger('scraper_logger')
 
 
 def get_url(url):
+    """ Get the URL using the driver object initialized at module level. This is to avoid initializing the driver
+    everytime we want to scrape a URL.
+
+    :param url: url to scrape
+    """
     d.driver.get(url)
 
 
@@ -15,10 +22,9 @@ class Scraper:
     def __init__(self, d: Driver):
         self.driver = d.driver
 
-    def get_title(self):
-        """
-        Get the brand and title of the product by first finding the brand and title element using the class name.
-        :param driver: driver with the product page loaded.
+    def get_title(self) -> str or None:
+        """ Get the brand and title of the product by first finding the brand and title element using the class name.
+
         :return: a string containing the brand and title of the product.
         """
         try:
@@ -30,10 +36,10 @@ class Scraper:
             return None
 
     # %%
-    def get_price(self):
-        """
-        Get the price of the product by first finding the price element using the class name
+    def get_price(self) -> str or None:
+        """ Get the price of the product by first finding the price element using the class name
          and then getting the text of the element.
+
         :return: a string containing the price of the product.
         """
         try:
@@ -45,11 +51,11 @@ class Scraper:
             return None
 
     # %%
-    def get_attributes(self):
+    def get_attributes(self) -> dict[str]:
         """
         Get the attributes of the product by first finding the attributes list element
         and then splitting the text and then pairing adjacent tokens as key-value pairs.
-        :param driver: driver with the product page loaded.
+
         :return: a dictionary containing the attributes of the product.
         """
         attributes = {}
@@ -64,7 +70,11 @@ class Scraper:
         finally:
             return attributes
 
-    def get_images(self):
+    def get_images(self) -> list[str]:
+        """ Get the images of the product by first finding the main image and then finding the other images.
+
+        :return: a list containing the images of the product.
+        """
         images = []
         try:
             main_image = self.driver.find_element(By.CLASS_NAME, 'base-product-image').find_element(By.TAG_NAME,
@@ -83,11 +93,11 @@ class Scraper:
         finally:
             return images
 
-    def get_description(self):
-        """
-        Get the description of the product by first finding the description list element and then iterating over the list items.
-        Initialize an empty string and append the text of each list item to the string using a new line seperator.
-        :param driver: driver with the product page loaded.
+    def get_description(self) -> str:
+        """ Get the description of the product by first finding the description list element
+        and then iterating over the list items. Initialize an empty string and append the text of each list item
+        to the string using a new line seperator.
+
         :return: a string containing the description of the product.
         """
         description = None
@@ -99,11 +109,33 @@ class Scraper:
         finally:
             return description
 
-    def get_barcode(self):
-        pass
+    def get_barcode(self) -> str or None:
+        """ Get the barcode from a script tag via a regex match.
+            Since barcode is mentioned in a script tag, we first find all script tags. Then, do a regex pattern match by
+            scanning the text inside the particular script tag.
+        :return: a string containing the barcode of the product or None if not found.
+        """
+        try:
+            # Find the script tag that contains the barcode information
+            script_tag = self.driver.find_element(By.XPATH, '/html/body/script[1]')
+            content = script_tag.get_attribute('innerHTML')
+            if 'window.__PRODUCT_DETAIL_APP_INITIAL_STATE__' in content:
+                # Use regular expressions to extract the barcode information
+                match = re.search(r'"barcode":"([a-zA-Z0-9]+)"', content)
+                if match:
+                    barcode = match.group(1)
+                    return barcode
+        except Exception as e:
+            pass
+        return None
 
 
 def scrape_data(scraper: Scraper) -> dict[str]:
+    """ Scrape the product details using the scraper object.
+
+    :param scraper: a scraper object
+    :return: a dictionary containing the product details
+    """
     return {
         'url': scraper.driver.current_url,
         'title': scraper.get_title(),
